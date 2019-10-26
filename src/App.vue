66ec6597dad1e54362951a193e3e278cde83d653
Farhia <farhia.fcb@gmail.com>
2019-10-26 16:16:13 +0200

    :construction: mobile first implementation

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,51 @@
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div> -->
+    </div>-->
     <the-header />
     <router-view />
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader'
+import TheHeader from "./components/TheHeader";
 export default {
   components: {
     TheHeader
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background: #ffc0cb url(assets/background.jpg);
 }
 
-#nav {
-  padding: 30px;
+#app {
+  width: 90%;
+  margin: 10px auto;
+  background: #fff;
+  box-shadow: #666666 0px 0px 7px;
+  overflow: hidden;
+  border-radius: 10px;
+  padding: 0 10px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+@media (min-width: 768px) and (max-width: 1023px) {
+  // tablet
+  #app {
+    width: 80%;
+    margin: 15px auto;
+    padding: 0 20px;
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media (min-width: 1024px) {
+  // desktop
+  #app {
+    width: 1000px;
+    margin: 20px auto;
+    padding: 0 20px;
   }
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,17 +1,13 @@
 <template>
   <header role="banner">
-      <img
-        src="/Users/farhia/Documents/dev 2019/cupcake-home-assignment/Assets/cupcakes_cookies.png"
-        alt="Cookie logo"/>
-    
     <h1 class="logo"><router-link to="/">Cupcakes & Cookies</router-link></h1>
     <nav role="navigation">
     <ul class="header-buttons/">
-      <li class="home"><router-link to="/">Home</router-link></li>
-      <li class="arrow"><router-link to="/">Arrow</router-link></li>
-      <li class="file"><router-link to="/">File</router-link></li>
-      <li class="faq"><router-link to="/">FAQ</router-link></li>
-      <li class="login"><router-link to="/">Profile</router-link></li>
+      <li class="icon-house"><router-link to="/">Home</router-link></li>
+      <li class="icon-compass-arrow"><router-link to="/">Arrow</router-link></li>
+      <li class="icon-file"><router-link to="/">File</router-link></li>
+      <li class="icon-question-mark"><router-link to="/">FAQ</router-link></li>
+      <li class="icon-person"><router-link to="/">Profile</router-link></li>
     </ul>
     </nav>
   </header>
@@ -24,4 +20,115 @@ export default {
 </script>
 
 <style>
+
+.logo a {
+  display: block;
+  height: 82px;
+  width: 282px;
+  background: url('../assets/cupcakes_cookies.png') 0 0 no-repeat;
+  text-indent: -9999px;
+}
+
+.header-buttons{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 10px;
+}
+
+.icon-house {
+    margin: 5px;
+    color: #FF4674;
+    background-image: url('../assets/Icons/house.png');
+    background-position: center;
+    height: 20px;
+    width: 20px;
+    right: 150px;
+    display: inherit;
+    right: 150px;
+}
+
+.icon-compass-arrow {
+    margin: 5px;
+    color: #FF4674;
+    background-image: url('../assets/Icons/compass-arrow.png');
+    height: 20px;
+    width: 20px;
+    right: 150px;
+    display: inherit;
+    right: 150px;
+}
+
+.icon-file {
+    margin: 5px;
+    color: #FF4674;
+    background-image: url('../assets/Icons/file.png');
+    height: 20px;
+    width: 20px;
+    right: 150px;
+    display: inherit;
+    right: 150px;
+}
+
+.icon-question-mark {
+    margin: 5px;
+    color: #FF4674;
+    background-image: url('../assets/Icons/question-mark.png');
+    height: 20px;
+    width: 20px;
+    right: 150px;
+    display: inherit;
+    right: 150px;
+}
+
+.icon-person {
+    margin: 5px;
+    color: #FF4674;
+    background-image: url('../assets/Icons/person.png');
+    height: 20px;
+    width: 20px;
+    right: 150px;
+    display: inherit;
+    right: 150px;
+}
+
+/*menu*/
+nav {
+  float: right;
+  margin: 20px 0px;
+}
+nav ul li {
+  background: #ee2461;
+  border-radius: 40px;
+  float: left;
+  margin-left: 10px;
+}
+nav ul li:hover {
+  box-shadow: #555555 0 0 6px;
+}
+nav ul li:first-child {
+  margin: 0;
+}
+nav ul li a {
+  text-indent: -9999px;
+  width: 42px;
+  height: 42px;
+  display: block;
+  background-repeat: no-repeat;
+  cursor: pointer;
+}
+nav ul li.home a {
+  background-position: center -1102px;
+}
+nav ul li.oQueE a {
+  background-position: center -830px;
+}
+nav ul li.planos a {
+  background-position: center -1170px;
+}
+nav ul li.faq a {
+  background-position: center -898px;
+}
+
+
 </style>
