b05004c4c5113dec6e3dfaa275e1caec736fe299
Farhia <farhia.fcb@gmail.com>
2019-10-26 17:23:06 +0200

    :construction: tablet and desktop implementation

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,101 +1,53 @@
 <template>
-  <header role="banner">
-    <h1 class="logo"><router-link to="/">Cupcakes & Cookies</router-link></h1>
+  <header class="header" role="banner">
+    <h1 class="logo">
+      <router-link to="/">Cupcakes & Cookies</router-link>
+    </h1>
     <nav role="navigation">
-    <ul class="header-buttons/">
-      <li class="icon-house"><router-link to="/">Home</router-link></li>
-      <li class="icon-compass-arrow"><router-link to="/">Arrow</router-link></li>
-      <li class="icon-file"><router-link to="/">File</router-link></li>
-      <li class="icon-question-mark"><router-link to="/">FAQ</router-link></li>
-      <li class="icon-person"><router-link to="/">Profile</router-link></li>
-    </ul>
+      <ul class="icon-buttons/">
+        <li class="icon-house"><router-link to="/">Home</router-link></li>
+        <li class="icon-compass-arrow"><router-link to="/">Arrow</router-link></li>
+        <li class="icon-file"><router-link to="/">File</router-link></li>
+        <li class="icon-question-mark"><router-link to="/">FAQ</router-link></li>
+        <li class="icon-person"><router-link to="/">Profile</router-link></li>
+      </ul>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'TheHeader'
+  name: "TheHeader"
 };
 </script>
 
-<style>
-
-.logo a {
-  display: block;
-  height: 82px;
-  width: 282px;
-  background: url('../assets/cupcakes_cookies.png') 0 0 no-repeat;
-  text-indent: -9999px;
+<style lang="scss">
+.header {
+  display: grid;
+  grid-template-rows: repeat(1, 1fr);
+  grid-gap: 20;
+  padding: 10px;
+  .logo {
+    margin: 0;
+    a {
+      display: block;
+      height: 82px;
+      width: 282px;
+      background: url("../assets/cupcakes_cookies.png") 0 0 no-repeat;
+      text-indent: -9999px;
+    }
+  }
 }
 
-.header-buttons{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-right: 10px;
-}
-
-.icon-house {
-    margin: 5px;
-    color: #FF4674;
-    background-image: url('../assets/Icons/house.png');
-    background-position: center;
-    height: 20px;
-    width: 20px;
-    right: 150px;
-    display: inherit;
-    right: 150px;
-}
-
-.icon-compass-arrow {
-    margin: 5px;
-    color: #FF4674;
-    background-image: url('../assets/Icons/compass-arrow.png');
-    height: 20px;
-    width: 20px;
-    right: 150px;
-    display: inherit;
-    right: 150px;
-}
-
-.icon-file {
-    margin: 5px;
-    color: #FF4674;
-    background-image: url('../assets/Icons/file.png');
-    height: 20px;
-    width: 20px;
-    right: 150px;
-    display: inherit;
-    right: 150px;
-}
-
-.icon-question-mark {
-    margin: 5px;
-    color: #FF4674;
-    background-image: url('../assets/Icons/question-mark.png');
-    height: 20px;
-    width: 20px;
-    right: 150px;
-    display: inherit;
-    right: 150px;
-}
-
-.icon-person {
-    margin: 5px;
-    color: #FF4674;
-    background-image: url('../assets/Icons/person.png');
-    height: 20px;
-    width: 20px;
-    right: 150px;
-    display: inherit;
-    right: 150px;
-}
-
-/*menu*/
 nav {
-  float: right;
-  margin: 20px 0px;
+  width: 100%;
+  ul {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 10px;
+    padding: 0;
+    margin: 0;
+  }
 }
 nav ul li {
   background: #ee2461;
@@ -103,12 +55,113 @@ nav ul li {
   float: left;
   margin-left: 10px;
 }
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  // tablet
+  .header {
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 10px;
+  align-items: center;
+  padding: 15px;
+  }
+}
+
+@media (min-width: 1024px) {
+  //Desktop
+  .header {
+  grid-template-areas: 
+    'logo . nav';
+  grid-template-columns: auto 1fr auto;
+  padding: 20px;
+  }
+
+  .logo{
+    grid-area: logo;
+  }
+
+  nav{
+    grid-area: nav;
+  }
+}
+
+.header-buttons {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 10px;
+}
+
+.icon-house {
+  margin: 5px;
+  color: #ff4674;
+  background-image: url("../assets/Icons/house.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 44px;
+  width: 44px;
+  right: 150px;
+  display: inherit;
+  right: 150px;
+}
+
+.icon-compass-arrow {
+  margin: 5px;
+  color: #ff4674;
+  background-image: url("../assets/Icons/compass-arrow.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 44px;
+  width: 44px;
+  right: 150px;
+  display: inherit;
+  right: 150px;
+}
+
+.icon-file {
+  margin: 5px;
+  color: #ff4674;
+  background-image: url("../assets/Icons/file.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 44px;
+  width: 44px;
+  right: 150px;
+  display: inherit;
+  right: 150px;
+}
+
+.icon-question-mark {
+  margin: 5px;
+  color: #ff4674;
+  background-image: url("../assets/Icons/question-mark.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 44px;
+  width: 44px;
+  right: 150px;
+  display: inherit;
+  right: 150px;
+}
+
+.icon-person {
+  margin: 5px;
+  color: #ff4674;
+  background-image: url("../assets/Icons/person.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 44px;
+  width: 44px;
+  right: 150px;
+  display: inherit;
+  right: 150px;
+}
+
+/*menu*/
 nav ul li:hover {
   box-shadow: #555555 0 0 6px;
 }
-nav ul li:first-child {
-  margin: 0;
-}
+
 nav ul li a {
   text-indent: -9999px;
   width: 42px;
@@ -129,6 +182,4 @@ nav ul li.planos a {
 nav ul li.faq a {
   background-position: center -898px;
 }
-
-
 </style>
