7082cfab220818dd8e29baccec72c1397fb978dc
Gloria Morales <gmoraleslondono@gmail.com>
2019-10-31 22:48:45 +0100

    Add component option

---
+++ src/components/Option.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="option">
+    <slot name="title"></slot>
+    <slot name="paragraph"></slot>
+    <slot name="list"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Option',
+  data() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
